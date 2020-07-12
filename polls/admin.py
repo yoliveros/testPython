c72c14f9d6ac2276935e189722a0b6790ac38166
Yoelvis Oliveros <yoelvisoliveros@gmail.com>
2020-07-12 17:33:47 -0400

    finish documentation tutorial

--- polls/admin.py
+++ polls/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
+from .models import Choice, Question
 
-# Register your models here.
-from django.contrib import admin
-from .models import Question
 
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+
+
+admin.site.register(Question, QuestionAdmin)
